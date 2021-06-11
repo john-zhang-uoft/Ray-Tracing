ad2675dcb85c93708696d1bedc95704faffa8c59
johnz <johnzhanguoft@gmail.com>
2021-06-11 12:24:47 -0400

    Sky gradient with ray

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <src/colour_gradient.h>
 
 int main() {
-    auto * gradient = new colour_gradient(200, 100, 255);
-    gradient->draw_gradient("Gradient.ppm");
+    auto * gradient = new colour_gradient(200, 100);
+    gradient->draw_diagonal_gradient("Gradient.ppm", 255);
+    gradient->draw_sky_background("Sky.ppm");
 }
--- src/colour_gradient.h
+++ src/colour_gradient.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include "vec3.h"
+#include "ray.h"
 
 using namespace std;
 
@@ -13,19 +14,20 @@ using namespace std;
 
 class colour_gradient {
 public:
-    colour_gradient(int x_size, int y_size, float default_blue = 0.2) {
+    colour_gradient(int x_size, int y_size) {
         x_pixels = x_size;
         y_pixels = y_size;
-        default_b = default_blue;
     }
+
     int x_pixels;
     int y_pixels;
-    float default_b;
 
-    inline void draw_gradient(const string& filename) const;
+    inline void draw_diagonal_gradient(const string &filename, float default_blue) const;
+    void draw_sky_background(const string &filename) const;
+    vec3 color(const ray& r) const;
 };
 
-inline void colour_gradient::draw_gradient(const string& filename) const  {
+inline void colour_gradient::draw_diagonal_gradient(const string &filename, float default_blue) const {
 // Create then open text file
     ofstream File(filename);
 
@@ -34,14 +36,46 @@ inline void colour_gradient::draw_gradient(const string& filename) const  {
     for (int y_ind = y_pixels - 1; y_ind >= 0; y_ind--) {
         for (int x_ind = 0; x_ind < x_pixels; x_ind++) {
             vec3 colour(float(x_ind) / float(x_pixels), float(y_ind) / float(y_pixels), 0);
-            int red = int(255.99 * colour[0]);
-            int green = int(255.99 * colour[1]);
-            int blue = int(default_b);
+            int r = int(255.99 * colour[0]);
+            int g = int(255.99 * colour[1]);
+            int b = int(default_blue);
 
-            File << red << " " << green << " " << blue << "\n";
+            File << r << " " << g << " " << b << "\n";
         }
     }
     File.close();
 }
+
+void colour_gradient::draw_sky_background(const string &filename) const {
+    ofstream File(filename);
+
+    File << "P3\n" << x_pixels << " " << y_pixels << "\n255\n";
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
+    for (int y_ind = y_pixels - 1; y_ind >= 0; y_ind--) {
+        for (int x_ind = 0; x_ind < x_pixels; x_ind++) {
+            float u = float(x_ind) / float(x_pixels);
+            float v = float(y_ind) / float(y_pixels);
+
+            ray ry(origin, lower_left_corner + u * horizontal + v * vertical);
+            vec3 col = color(ry);
+            int r = int(255.99 * col[0]);
+            int g = int(255.99 * col[1]);
+            int b = int(255.99 * col[2]);
+
+            File << r << " " << g << " " << b << "\n";
+        }
+    }
+
+}
+
+vec3 colour_gradient::color(const ray &r) const {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
 
 #endif //RAY_TRACING_COLOUR_GRADIENT_H
